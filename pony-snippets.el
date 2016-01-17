7a30c97582489b224346551ab8028484e9bb8bc4
Sean T Allen <sean@monkeysnatchbanana.com>
2016-01-17 07:39:28 -0500

    ¯\_(ツ)_/¯

--- pony-snippets.el
+++ pony-snippets.el
@@ -1,8 +1,7 @@
 ;;; pony-snippets.el --- Yasnippets for Pony
 
-;; Author: Sean T Allen <sean@monkeysnatchbanana.com>
-;; URL: https://github.com/haskell/haskell-snippets
-;; Keywords: snippets, pony
+;; Copyright (C) 2016 Sean T. Allen
+;; URL: https://github.com/seantallen/pony-snippets
 ;; Version: 0.0.1
 ;; Package-Requires: ((yasnippet "0.8.0"))
 
