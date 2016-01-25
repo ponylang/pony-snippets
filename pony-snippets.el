4f0736c01c36622d0a75ad630520fbadff385bbe
Sean T Allen <sean@monkeysnatchbanana.com>
2016-01-24 19:34:46 -0500

    Update available snippets listing.
    These are first pass. Going to be looking to
    cut down on number of keys needed to expand.

--- pony-snippets.el
+++ pony-snippets.el
@@ -11,8 +11,33 @@
 ;;
 ;;   Available Expansion Keys:
 ;;
-;;       trye - try/else/end block
-;;       use  - use module
+;;       actor     - actor definition
+;;       be        - behavior definition
+;;       class     - class definition
+;;       for       - for/end block
+;;       fore      - for/else/end block
+;;       if        - if/end block
+;;       ife       - if/else/end block
+;;       interface - interface definition
+;;       lambda    - lambda definition
+;;       match     - match/end block
+;;       matche    - match/else/end block
+;;       new       - new method definition
+;;       newc      - new create method definition
+;;       object    - object literal definition
+;;       primitive - primitive definition
+;;       recover   - recover block
+;;       repeat    - repeat/until/end block
+;;       repeate   - repeat/until/else/end block
+;;       struct    - struct definition
+;;       trait     - trait definition
+;;       try       - try/end block
+;;       trye      - try/else/end block
+;;       type      - type definition
+;;       use       - use module
+;;       while     - while/end block
+;;       whilee    - while/else/end block
+;;       with      - with/end block
 ;;
 
 ;;; Code:
